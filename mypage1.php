0cc0e2ae820e9e413bf1905b74a31b6d8681288d
Alsomesin <97584431+Alsomesin@users.noreply.github.com>
2022-02-11 13:49:41 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -3,4 +3,7 @@
     <title>การเชื่อมโยงเว็บไซต์</title>
   </head>
   <body>
-    <a href="https://m.facebook.com/"><h1>วาร์ปของฉัน</h1></a>
+    <a href="https://m.facebook.com/วาร์ปของฉัน</h1></a>
+    <a href="mypage2.php"><h1>ไปpage2</h1></a>
+  </body>
+</html>
