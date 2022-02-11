e972b7e02a498a58d0300e7a6e89ed1e7052403e
Alsomesin <97584431+Alsomesin@users.noreply.github.com>
2022-02-11 13:59:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-
+<html>
+  <head>
+    <title>การเชื่อมโยงเว็บไซต์</title>
+  </head>
+  <body>
+    <a href="https://m.facebook.com/"><h1>วาร์ปของฉัน</h1></a>
+    <a href="mypage2.php"><h1>ไปpage2</h1></a>
+  </body>
+</html>
