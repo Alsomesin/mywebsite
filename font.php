3efec06ac77b3d6f4c7a51e66fac2667b00e14e1
Alsomesin <97584431+Alsomesin@users.noreply.github.com>
2022-02-23 14:03:06 +0700

    Update font.php

--- font.php
+++ font.php
@@ -8,7 +8,7 @@
       }
       @font-face{
         font-family: "MyFont2";
-        src: url("baozi.ttf");
+        src: url("ChocolateteehaypaiTEST.ttf");
       }
       #font1{
         font-family: "MyFont1";
