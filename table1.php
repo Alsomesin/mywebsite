c340c20f4f6803b7bdc733deed9c264d6c234616
Alsomesin <97584431+Alsomesin@users.noreply.github.com>
2022-03-02 15:35:05 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -12,9 +12,9 @@
       </tr>
       <tr>
         <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
-        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+        <td>Beomgyu</td>
+        <td><img src="https://pin.it/3y8cZaG" width="45px"></td>
+        <td><a href="https://twitter.com/TXT_members?t=K4sEFfsZE3UXrQmgs4Uxrg&s=09">Twitter</a></td>
       </tr>
     </table>
   </body>
