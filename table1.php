9c985b3c5f260c309166626f39eed09eb711f9c0
Alsomesin <97584431+Alsomesin@users.noreply.github.com>
2022-03-02 17:17:25 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -8,13 +8,37 @@
         <th>ลำดับ</th>
         <th>ชื่อ</th>
         <th>รูปภาพ</th>
-        <th>Twitter/FB</th>
+        <th>Instragram</th>
       </tr>
       <tr>
         <td>1</td>
-        <td>Beomgyu</td>
-        <td><img src="https://pin.it/3y8cZaG" width="45px"></td>
-        <td><a href="https://twitter.com/TXT_members?t=K4sEFfsZE3UXrQmgs4Uxrg&s=09">Twitter</a></td>
+        <td>กงยู</td>
+        <td><img src="https://www.instagram.com/p/CX-SMFtpy7U/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/gongyoo_official?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>ปาร์คแทจุน</td>
+        <td><img src="https://www.instagram.com/p/CTW5xOglpSZ/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/paktaejun?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>คิมแจฮัน</td>
+        <td><img src="https://www.instagram.com/p/CahFIi5l-Hs/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/jjanstagram?utm_medium=copy_link">Instagram</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>ลูคัส</td>
+        <td><img src="https://www.instagram.com/p/CPI7sEBByUS/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/lucas_xx444?utm_medium=copy_link">Instagram</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>มิลลี บ็อบบี บราวน์</td>
+        <td><img src="https://www.instagram.com/p/CXTd4W7lCOX/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/milliebobbybrown?utm_medium=copy_link">Instagram</a></td>
       </tr>
     </table>
   </body>
