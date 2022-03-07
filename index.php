06a7c0394bc076c85d33c3486cd8014e5c201c03
Alsomesin <97584431+Alsomesin@users.noreply.github.com>
2022-03-07 22:38:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,45 @@
 <html>
   <head>
-    <title>page1</title>
+    <title>การสร้างตาราง</title>
   </head>
-  <body bgcolor="lightblue">
-    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
-    <p><b>ชื่อ:</b> <i>นางสาวญาณดา ทองขาน</i></p>
-    <p><b>ชื่อเล่น:</b> <i>ออมสิน</i></p>
-    <p><b>ชั้น:</b> <i>ม.4/12</i></p>
-    <p><b>เลขที่:</b> <i>15</i></p>
-    <p><b>เลขประจำตัว:</b> <i>57054</i></p>
-    <p><b>โรงเรียน:</b> <i>หล่มสักวิทยาคม</i></p>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Instragram</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>กงยู</td>
+        <td><img src="https://www.instagram.com/p/CX-SMFtpy7U/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/gongyoo_official?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>ปาร์คแทจุน</td>
+        <td><img src="https://www.instagram.com/p/CTW5xOglpSZ/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/paktaejun?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>คิมแจฮัน</td>
+        <td><img src="https://www.instagram.com/p/CahFIi5l-Hs/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/jjanstagram?utm_medium=copy_link">Instagram</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>ลูคัส</td>
+        <td><img src="https://www.instagram.com/p/CPI7sEBByUS/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/lucas_xx444?utm_medium=copy_link">Instagram</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>มิลลี บ็อบบี บราวน์</td>
+        <td><img src="https://www.instagram.com/p/CXTd4W7lCOX/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/milliebobbybrown?utm_medium=copy_link">Instagram</a></td>
+      </tr>
+    </table>
   </body>
 </html>
